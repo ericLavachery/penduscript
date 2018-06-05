9aa0d7300dd6954dc77eb8ef47a1f1dbcd5fc0f2
Eric Lavachery <eric.lavachery@gmail.com>
2018-06-05 11:43:20 +0200

    cookie avec mots trouvés

--- pendu.js
+++ pendu.js
@@ -1,32 +1,76 @@
 
+// create cookie function
+function createCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// get cookie function
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+// get cookie with done words
+var dwJSON = getCookie('dwCookie');
+if (dwJSON !== '') {
+    var doneWords = JSON.parse(dwJSON);
+} else {
+    var doneWords = [];
+}
+
+// console.log(doneWords);
+
+// reset
 var goodLetters = [];
 var foundLetters = [];
 var restLetters = [];
 
+// prompt for new word
 var yourInput = prompt("Add your own word, or leave empty for a random word", "");
 
+// capitalize the new word if there is one
 if (typeof yourInput !== 'object') {
-
     if (!/[^a-z]/i.test(yourInput)) {
         yourEntry = yourInput.toUpperCase();
     } else {
         yourEntry = '';
     }
-
 } else {
     yourEntry = '';
 }
 
 // console.log("your entry = " + yourEntry);
 
+// take a random word in an array if no word was entered
 if (yourEntry == '' || yourEntry == null) {
     var myWords = [
         "BONJOUR",
+        "TYRANNOSAURE",
         "CAOUTCHOUC",
         "OISEAU",
+        "HYDROPHOBE",
+        "SCHISME",
+        "COCCYX",
+        "WHISKY",
+        "CABINET",
+        "PLAFOND",
+        "JALOUX",
         "JAVASCRIPT",
         "SCHTROUMPF",
-        "TYRANNOSAURE",
         "FLUORESCENCE",
         "PARTITION",
         "SULTAN",
@@ -36,6 +80,8 @@ if (yourEntry == '' || yourEntry == null) {
         "TRIANGLE",
         "PERDITION",
         "NOMENCLATURE",
+        "DESHABILLER",
+        "CRAYON",
         "FILANDREUX",
         "FASTOCHE",
         "MICROBIOTIQUE",
@@ -46,22 +92,36 @@ if (yourEntry == '' || yourEntry == null) {
         "MYRIADE",
         "ORNITHOPTERE",
         "SALTIMBANQUE",
+        "APPUYER",
         "DUPLEX",
         "ZERO",
         "USURPER",
         "AMYGDALES",
         "ASTHME",
         "GNOU",
-        "YGTHOMURAJODOLUW"
+        "YGTHOMURAJADOLUW"
     ];
+
+    // take a random word and repeat if word is already done
+    var ii = 0;
+    while (ii <= 200) {
+        var myWord = myWords[Math.floor(Math.random()*myWords.length)];
+        // console.log(myWord);
+        if (doneWords.includes(myWord) == false) {break;}
+        ii = ii + 1;
+    }
+
     // choose a word
-    var myWord = myWords[Math.floor(Math.random()*myWords.length)];
+    // var myWord = myWords[Math.floor(Math.random()*myWords.length)];
+    // console.log("Word = " + myWord);
+
 } else {
     var myWord = yourEntry;
 }
 
 // console.log(myWord);
 
+// makes goodLetters, restLetters and foundLetters arrays
 var i = 0;
 while (i != myWord.length) {
     goodLetters.push(myWord.substr(i, 1));
@@ -70,6 +130,7 @@ while (i != myWord.length) {
     i = i + 1;
 }
 
+// reset testedLetters and potence
 var testedLetters = [];
 var potence = 0;
 
@@ -77,6 +138,7 @@ var potence = 0;
 document.getElementById("wordpart").innerHTML = foundLetters.join(" ");
 document.getElementById("tested").innerHTML = "Tested letters: /";
 
+// THE GAME
 function guessLetter() {
     var inLetter = prompt("Please type a letter", "");
     var yourLetter = inLetter.toUpperCase();
@@ -146,6 +208,10 @@ function guessLetter() {
             document.getElementById("pendu").innerHTML = '<img src="img/pendu' + potence + '.png" alt="">';
         } else {
             document.getElementById("won").innerHTML = '<span style="color: blue;">YEEEEHEEEEE!!!</span>';
+            // Rajouter le mot trouvé dans le cookie
+            doneWords.push(myWord);
+            var dwJSON = JSON.stringify(doneWords);
+            createCookie('dwCookie', dwJSON, 60);
         }
 
     document.getElementById("tell").innerHTML = response;
